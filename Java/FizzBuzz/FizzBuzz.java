026e2ea8dbd8e64b028e212ffcbb0171fd86542d
Tai <tanphamtai@outlook.de>
2023-02-28 12:47:38 +0100

    Input with scanner

--- Java/FizzBuzz/FizzBuzz.java
+++ Java/FizzBuzz/FizzBuzz.java
@@ -1,24 +1,21 @@
+import java.util.Scanner;
+
 public class FizzBuzz 
+
 {
     public static void main (String[] args)
     {
-        for(int i=0; i <= 16; i++) 
-        {
+        Scanner scan = new Scanner(System.in);
+        
+        while (!scan.hasNextInt()) {
+          System.out.println("Input is not a number.");
+          scan.nextLine();
+        }
 
-            //Tais Lösung
-            /* 
-            if(i%3==0)
-            {
-                if (i%5==0) 
-                {
-                    System.out.println("Fizz Buzz"); 
-                }
-                else
-                    {
-                    System.out.println("Fizz");
-                    }
-            }
-            */
+        int countto = scan.nextInt();
+
+        for(int i=0; i <= countto; i++) 
+        {
 
             if(i%3==0 && i%5==0)
             {
