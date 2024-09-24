5d43d90623f67a47130c11ee36f044f7b317315e
Tai <72919971+taizen01@users.noreply.github.com>
2024-09-24 16:47:49 +0200

    thank you

--- Python/Learning/neg_numbers/negative_numbers_neg.py
+++ Python/Learning/neg_numbers/negative_numbers_neg.py
@@ -7,18 +7,19 @@ os.system('cls')
 GRUEN = "\033[92m"
 GELB = "\033[33m"
 RESET = "\033[0m"
+LINE = "==="
 
 
 ganzahl = int(input("Bitte gib eine positive Ganzzahl ein: ")) 
 if ganzahl > 0:
-    print("----------------------------------------------------------")
+    print(f'{LINE:=^45}')
 else:
     print("Zahl war nicht positiv") 
     exit()
 
 print(GELB +"\nAls Dezimalzahl: " + str(ganzahl) + "\n")
 print("In Bitdarstellung: " + bit_length(ganzahl) + "\n" + RESET)
-print("----------------------------------------------------------\n")
+print(f'{LINE:=^45}')
 time.sleep(1)
 print("Negiere alle Bits...\n")
 time.sleep(1)
@@ -31,9 +32,9 @@ result = bin(int(bin_convert(bit_length(ganzahl)),2) +1)
 
 print("Addiere Binär 1 auf die negierte Binärzahl ...\n")
 
-print("----------------------------------------------------------\n")
+print(f'{LINE:=^45}')
 print(GRUEN + "In Bitdarstellung: " + str(result[2:]) + "\n")
 print("Dezimalzahl der Umwandlung: " + str(twos_complement_to_decimal(str(result[2:]))) + RESET + "\n")
     
 print("Länge der Bits der Binärzahl: " + str(len(str(result[2:]))) + "bits\n")
-print("----------------------------------------------------------")
+print(f'{LINE:=^45}')
--- Python/Learning/slicing.py
+++ Python/Learning/slicing.py
@@ -1,1 +1,15 @@
-print(0b00000110)
+eingabe = "Titanic"
+length = (len(eingabe))
+
+print("----------------\n")
+
+for i in range(length):
+    print(eingabe[i]  + " : " + str(i) + " :  " + str(0-(length-i)))
+
+print("----------------\n")
+
+method1 = eingabe[6]
+method2 = eingabe[-1]
+method3 = eingabe[6:]
+
+print(f'{method1} {method2} {method3}')
